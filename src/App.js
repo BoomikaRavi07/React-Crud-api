39b3a51d376b6e5256a6e1420b45694626ecb67c
Boomika <boomikaravi0705@gmail.com>
2024-02-05 10:01:49 +0530

    crud prg

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RestController from './RestController';
+
+function App() {
+  return (
+    <RestController/>
+  );
+}
+
+export default App;
+/*
+import RestControllerGet from './RestControllerGet';
+  <RestControllerGet/>*/
